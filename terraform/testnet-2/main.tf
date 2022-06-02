53b0e9a908f300a1b1bde5a243623ee6eff86966
Sirapop Na Ranong <n.r.sirapop@gmail.com>
2022-06-02 12:58:35 +0700

    update terraform

--- terraform/testnet-2/main.tf
+++ terraform/testnet-2/main.tf
@@ -118,11 +118,15 @@ resource "aws_lambda_function" "function" {
       LOG_LEVEL                 = var.log_level
     }
   }
+  kms_key_arn      = ""
 }
 
 resource "aws_apigatewayv2_api" "api" {
   name          = "${var.package_name}-${var.environment}-api"
   protocol_type = "HTTP"
+  cors_configuration {
+    allow_origins = ["*"]
+  }
   route_key     = "ANY /${aws_lambda_function.function.function_name}"
   target        = aws_lambda_function.function.arn
 }
