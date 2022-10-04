705f916515538678d5194bf8106949fccc6251d2
Sirapop Na Ranong <n.r.sirapop@gmail.com>
2022-10-04 12:23:49 +0700

    update inflation equation

--- functions/api/services/inflation.js
+++ functions/api/services/inflation.js
@@ -1,4 +1,5 @@
 const config = require('config-yml');
+const _ = require('lodash');
 const lcd = require('./lcd');
 const cli = require('./cli');
 const {
@@ -14,7 +15,29 @@ module.exports = async (
 ) => {
   let response;
 
-  const numEVMChains = evm_chains_data.length;
+  let {
+    uptimeRate,
+    heartbeatRate,
+    numEVMChains,
+    unsubmittedVoteRates,
+  } = { ...params };
+
+  uptimeRate = uptimeRate ||
+    1;
+  heartbeatRate = heartbeatRate ||
+    1;
+  numEVMChains = numEVMChains ||
+    evm_chains_data.length;
+  unsubmittedVoteRates = unsubmittedVoteRates ||
+    Object.fromEntries(
+      evm_chains_data
+        .map(c =>
+          [
+            c?.id,
+            0,
+          ]
+        )
+    );
 
   response = await lcd(
     '/cosmos/mint/v1beta1/inflation',
@@ -22,6 +45,14 @@ module.exports = async (
 
   const tendermintInflationRate = response ?
     Number(response.inflation) :
+    null;
+
+  response = await lcd(
+    '/cosmos/distribution/v1beta1/params',
+  );
+
+  const communityTax = response ?
+    Number(response.params?.community_tax) :
     null;
 
   response = await cli(
@@ -62,23 +93,32 @@ module.exports = async (
 
   const inflation = parseFloat(
     (
-      (
-        tendermintInflationRate *
-        (1 + keyMgmtRelativeInflationRate)
-      ) +
+      (uptimeRate * (tendermintInflationRate || 0) * (1 - (communityTax || 0))) +
+      (heartbeatRate * (keyMgmtRelativeInflationRate || 0) * (tendermintInflationRate || 0)) +
       (
         externalChainVotingInflationRate *
-        numEVMChains
+        _.sum(
+          Object.values({ ...unsubmittedVoteRates })
+            .map(v => 1 - v)
+        )
       )
     )
     .toFixed(6)
   );
 
   return {
-    equation: 'inflation = (tendermintInflationRate * (1 + keyMgmtRelativeInflationRate)) + (externalChainVotingInflationRate * numEVMChains)',
+    equation: `inflation = (uptimeRate * tendermintInflationRate * (1 - communityTax)) + (heartbeatRate * keyMgmtRelativeInflationRate * tendermintInflationRate) + (externalChainVotingInflationRate * (${evm_chains_data
+      .map(c => `(1 - ${c?.id}UnsubmittedVoteRate)`)
+      .join(' + ')
+    }))`,
     tendermintInflationRate,
+    communityTax,
     keyMgmtRelativeInflationRate,
     externalChainVotingInflationRate,
+    uptimeRate,
+    heartbeatRate,
+    numEVMChains,
+    unsubmittedVoteRates,
     numEVMChains,
     inflation,
   };
