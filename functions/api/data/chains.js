baf0aa4dc7cfe1f55a2fc47fb8e26c3385d0e838
Sirapop Na Ranong <n.r.sirapop@gmail.com>
2023-01-22 11:50:38 +0700

    remove binance rpcs

--- functions/api/data/chains.js
+++ functions/api/data/chains.js
@@ -1234,9 +1234,6 @@ module.exports = {
             chainId: '0x61',
             chainName: 'BNB Chain Testnet',
             rpcUrls: [
-              'https://data-seed-prebsc-1-s1.binance.org:8545',
-              'https://data-seed-prebsc-2-s1.binance.org:8545',
-              'https://data-seed-prebsc-1-s2.binance.org:8545',
               'https://bsc-testnet.public.blastapi.io',
               'https://rpc.ankr.com/bsc_testnet_chapel',
             ],
