160907948ac40d8caeb6d6deb8543ac94069a9e8
Sirapop Na Ranong <n.r.sirapop@gmail.com>
2023-06-04 11:48:45 +0700

    feat: use utc for granularity

--- functions/api/utils/time.js
+++ functions/api/utils/time.js
@@ -1,7 +1,7 @@
 const moment = require('moment');
 
 const getGranularity = timestamp => {
-  const time = moment(timestamp);
+  const time = moment(timestamp).utc();
   return {
     ms: time.valueOf(),
     ...Object.fromEntries(['hour', 'day', 'week', 'month', 'quarter', 'year'].map(x => [x, time.startOf(x).valueOf()])),
