27d3f0c97572e532548db463205a275b5126bd5b
RiceAndmeet <jun.ying.yan@ryerson.ca>
2023-06-13 20:57:58 -0400

    feat: stagenet terraform

---
+++ terraform/stagenet/main.tf
@@ -0,0 +1,185 @@
+terraform {
+  backend "s3" {
+    bucket = "axelar-terraform"
+    key    = "services/axelarscan-api/stagenet/terraform.tfstate"
+    region = "us-east-2"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.14"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
+
+provider "aws" {
+  region  = var.aws_region
+}
+
+provider "archive" {}
+
+data "archive_file" "zip_api" {
+  type        = "zip"
+  source_dir  = "../../functions/api"
+  excludes    = ["yarn.lock", ".env.example", ".env", "test"]
+  output_path = "${var.project_name}-api.zip"
+}
+
+data "aws_iam_policy_document" "policy" {
+  statement {
+    sid     = ""
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      identifiers = ["lambda.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+}
+
+data "aws_iam_role" "role" {
+  name = "${var.iam_role}"
+}
+
+resource "aws_iam_policy_attachment" "attachment" {
+  name       = "${var.project_name}-attachment"
+  roles      = [data.aws_iam_role.role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "api" {
+  function_name    = "${var.project_name}-${var.environment}"
+  filename         = data.archive_file.zip_api.output_path
+  source_code_hash = data.archive_file.zip_api.output_base64sha256
+  role             = data.aws_iam_role.role.arn
+  handler          = "index.handler"
+  runtime          = "nodejs14.x"
+  timeout          = 30
+  memory_size      = 512
+  publish          = true
+  environment {
+    variables = {
+      NODE_NO_WARNINGS           = 1
+      ENVIRONMENT                = var.environment
+      GENERAL_INDEXER_URL        = var.general_indexer_url
+      GENERAL_INDEXER_USERNAME   = var.general_indexer_username
+      GENERAL_INDEXER_PASSWORD   = var.general_indexer_password
+      TRANSFERS_INDEXER_URL      = var.transfers_indexer_url
+      TRANSFERS_INDEXER_USERNAME = var.transfers_indexer_username
+      TRANSFERS_INDEXER_PASSWORD = var.transfers_indexer_password
+      LOG_LEVEL                  = var.log_level
+    }
+  }
+  kms_key_arn      = ""
+}
+
+resource "aws_lambda_provisioned_concurrency_config" "config" {
+  function_name                     = aws_lambda_function.api.function_name
+  provisioned_concurrent_executions = 100
+  qualifier                         = aws_lambda_function.api.version
+}
+
+resource "aws_apigatewayv2_api" "api" {
+  name          = "${var.project_name}-${var.environment}-api"
+  protocol_type = "HTTP"
+  cors_configuration {
+    allow_origins = ["*"]
+    allow_headers = ["*"]
+    allow_methods = ["*"]
+  }
+  route_key     = "ANY /${aws_lambda_function.api.function_name}"
+  target        = aws_lambda_function.api.arn
+}
+
+resource "aws_apigatewayv2_route" "route_default" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "ANY /"
+  target    = "integrations/${var.api_gateway_integration_id}"
+}
+
+resource "aws_apigatewayv2_route" "route_cross-chain" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "ANY /cross-chain/{function}"
+  target    = "integrations/${var.api_gateway_integration_id}"
+}
+
+resource "aws_apigatewayv2_route" "route_function" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "ANY /{function}"
+  target    = "integrations/${var.api_gateway_integration_id}"
+}
+
+data "archive_file" "zip_axelar_crawler" {
+  type        = "zip"
+  source_dir  = "../../functions/axelar-crawler"
+  excludes    = ["yarn.lock", ".env.example", ".env", "local.js"]
+  output_path = "${var.project_name}-axelar-crawler.zip"
+}
+
+resource "aws_lambda_function" "axelar_crawler" {
+  function_name    = "${var.project_name}-axelar-crawler-${var.environment}"
+  filename         = data.archive_file.zip_axelar_crawler.output_path
+  source_code_hash = data.archive_file.zip_axelar_crawler.output_base64sha256
+  role             = data.aws_iam_role.role.arn
+  handler          = "index.handler"
+  runtime          = "nodejs14.x"
+  timeout          = 900
+  memory_size      = 1536
+  environment {
+    variables = {
+      NODE_NO_WARNINGS = 1
+      ENVIRONMENT      = var.environment
+      LOG_LEVEL        = var.log_level
+    }
+  }
+  kms_key_arn      = ""
+}
+
+resource "aws_cloudwatch_event_rule" "schedule_axelar_crawler" {
+  name                = "${var.project_name}-axelar-crawler-${var.environment}-rule"
+  schedule_expression = "cron(*/10 * * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "target_axelar_crawler" {
+  rule      = aws_cloudwatch_event_rule.schedule_axelar_crawler.name
+  target_id = aws_lambda_function.axelar_crawler.id
+  arn       = aws_lambda_function.axelar_crawler.arn
+}
+
+data "archive_file" "zip_evm_crawler" {
+  type        = "zip"
+  source_dir  = "../../functions/evm-crawler"
+  excludes    = ["yarn.lock", ".env.example", ".env", "local.js", "sync.js"]
+  output_path = "${var.project_name}-evm-crawler.zip"
+}
+
+resource "aws_lambda_function" "evm_crawler" {
+  function_name    = "${var.project_name}-evm-crawler-${var.environment}"
+  filename         = data.archive_file.zip_evm_crawler.output_path
+  source_code_hash = data.archive_file.zip_evm_crawler.output_base64sha256
+  role             = data.aws_iam_role.role.arn
+  handler          = "index.handler"
+  runtime          = "nodejs14.x"
+  timeout          = 630
+  memory_size      = 1536
+  environment {
+    variables = {
+      NODE_NO_WARNINGS = 1
+      ENVIRONMENT      = var.environment
+      LOG_LEVEL        = var.log_level
+    }
+  }
+  kms_key_arn      = ""
+}
+
+resource "aws_cloudwatch_event_rule" "schedule_evm_crawler" {
+  name                = "${var.project_name}-evm-crawler-${var.environment}-rule"
+  schedule_expression = "cron(*/10 * * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "target_evm_crawler" {
+  rule      = aws_cloudwatch_event_rule.schedule_evm_crawler.name
+  target_id = aws_lambda_function.evm_crawler.id
+  arn       = aws_lambda_function.evm_crawler.arn
+}
---
+++ terraform/stagenet/variables.tf
@@ -0,0 +1,59 @@
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-east-2"
+}
+
+variable "iam_role" {
+  description = "AWS IAM role"
+  default     = ""
+}
+
+variable "project_name" {
+  description = "Project name"
+  default     = "axelarscan"
+}
+
+variable "environment" {
+  description = "Environment"
+  default     = "stagenet"
+}
+
+variable "general_indexer_url" {
+  description = "General indexer url"
+  default     = ""
+}
+
+variable "general_indexer_username" {
+  description = "General indexer username"
+  default     = ""
+}
+
+variable "general_indexer_password" {
+  description = "General indexer password"
+  default     = ""
+}
+
+variable "transfers_indexer_url" {
+  description = "Transfers indexer url"
+  default     = ""
+}
+
+variable "transfers_indexer_username" {
+  description = "Transfers indexer username"
+  default     = ""
+}
+
+variable "transfers_indexer_password" {
+  description = "Transfers indexer password"
+  default     = ""
+}
+
+variable "api_gateway_integration_id" {
+  description = "API gateway integration id"
+  default     = ""
+}
+
+variable "log_level" {
+  description = "Log level"
+  default     = "debug"
+}
