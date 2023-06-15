854d41dd60a871e52e25143f2df15b69894f60b5
Sirapop Na Ranong <n.r.sirapop@gmail.com>
2023-06-15 12:46:38 +0800

    chore: update terraform

--- terraform/mainnet/main.tf
+++ terraform/mainnet/main.tf
@@ -58,7 +58,7 @@ resource "aws_opensearch_domain" "domain" {
   ebs_options {
     ebs_enabled = true
     volume_type = "gp2"
-    volume_size = 320
+    volume_size = 448
   }
   encrypt_at_rest {
     enabled = true
