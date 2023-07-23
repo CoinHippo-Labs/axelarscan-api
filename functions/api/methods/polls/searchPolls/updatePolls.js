55690eafd02ca2e1403ca6501709c41adf041828
Sirapop Na Ranong <n.r.sirapop@gmail.com>
2023-07-23 19:01:33 +0700

    feat: update polls

--- functions/api/methods/polls/searchPolls/updatePolls.js
+++ functions/api/methods/polls/searchPolls/updatePolls.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+
+const lcd = require('../../lcd');
 const { write } = require('../../../services/index');
 const { getChainData } = require('../../../utils/config');
 const { toArray } = require('../../../utils');
@@ -14,7 +17,7 @@ module.exports = async (collection, data, params) => {
         toArray(data).map(d =>
           new Promise(
             async resolve => {
-              const { id, success, failed, confirmation, num_recover_time } = { ...d };
+              const { id, height, success, failed, confirmation, participants, num_recover_time } = { ...d };
               let _updated;
               if (!failed && !(success || confirmation) && Object.entries({ ...d }).filter(([k, v]) => k.startsWith(prefix_address) && !v?.vote).length > MIN_VOTE_PER_POLL_TO_FAILED) {
                 d.failed = true;
@@ -23,6 +26,17 @@ module.exports = async (collection, data, params) => {
               if (status === 'to_recover' && !d.failed) {
                 d.num_recover_time = (num_recover_time || 0) + 1;
                 _updated = true;
+              }
+              if (data.length === 1 && height && success && Object.entries({ ...d }).filter(([k, v]) => k.startsWith(prefix_address)).length < toArray(participants).length) {
+                const heights = _.orderBy(_.range(-1, 3).map(i => height + i), [], ['desc']);
+                for (const height of heights) {
+                  let next_key = true;
+                  while (next_key) {
+                    const page_key = typeof next_key === 'string' && next_key ? next_key : undefined;
+                    const response = await lcd('/cosmos/tx/v1beta1/txs', { index: true, index_poll: true, events: `tx.height=${height}`, 'pagination.key': page_key });
+                    next_key = response?.pagination?.next_key;
+                  }
+                }
               }
               if (_updated) {
                 await write(collection, id, d, true);
--- functions/axelar-crawler/services/updatePolls/index.js
+++ functions/axelar-crawler/services/updatePolls/index.js
@@ -9,14 +9,16 @@ module.exports = async context => {
     const service_name = `${!context ? 'local_' : ''}axelarscan-axelar-crawler`;
     const method = 'updatePolls';
     const prefix_address = 'axelar';
+    const getHeights = data => _.uniq(toArray(toArray(data).map(d => _.min(toArray(_.concat(d.height, Object.entries(d).filter(([k, v]) => k.startsWith(`${prefix_address}1`) && v?.height).map(([k, v]) => v.height)))))));
 
     while (!context || context.getRemainingTimeInMillis() > 300 * 1000) {
       // log('info', service_name, `start ${method}`);
       // await api.get('/', { params: { method } }).catch(error => parseRequestError(error));
       // log('info', service_name, `end ${method}`);
-      const response = await api.get('/', { params: { method: 'searchPolls', status: 'to_recover', size: 15, sort: [{ 'created_at.ms': 'asc' }] } }).catch(error => parseRequestError(error));
-      const { data } = { ...response?.data };
-      let heights = _.uniq(toArray(toArray(data).map(d => _.min(toArray(_.concat(d.height, Object.entries(d).filter(([k, v]) => k.startsWith(`${prefix_address}1`) && v?.height).map(([k, v]) => v.height)))))));
+      let response = await api.get('/', { params: { method: 'searchPolls' } }).catch(error => parseRequestError(error));
+      let heights = getHeights(toArray(response?.data?.data).filter(d => d.success));
+      response = await api.get('/', { params: { method: 'searchPolls', status: 'to_recover', size: 15, sort: [{ 'created_at.ms': 'asc' }] } }).catch(error => parseRequestError(error));
+      heights = _.uniq(_.concat(heights, getHeights(response?.data?.data));
       heights = _.orderBy(_.uniq(heights.flatMap(h => _.range(-1, 6).map(i => h + i))), [], ['desc']);
 
       for (const height of heights) {
@@ -24,12 +26,12 @@ module.exports = async context => {
         let next_key = true;
         while (next_key) {
           const page_key = typeof next_key === 'string' && next_key ? next_key : undefined;
-          const response = await api.post('/', { method: 'lcd', path: '/cosmos/tx/v1beta1/txs', index: true, index_transfer: true, index_poll: true, events: `tx.height=${height}`, 'pagination.key': page_key }).catch(error => parseRequestError(error));
+          const response = await api.post('/', { method: 'lcd', path: '/cosmos/tx/v1beta1/txs', index: true, index_poll: true, events: `tx.height=${height}`, 'pagination.key': page_key }).catch(error => parseRequestError(error));
           next_key = response?.data?.pagination?.next_key;
         }
         // log('info', service_name, `end ${method}`, { height });
       }
-      await sleep(10 * 1000);
+      await sleep(3 * 1000);
     }
   }
 };
