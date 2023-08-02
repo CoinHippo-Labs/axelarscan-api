e5f30d821fb66afab298934fee118d220065e204
Sirapop Na Ranong <n.r.sirapop@gmail.com>
2023-08-03 01:02:20 +0700

    chore: fix import

--- functions/api/methods/interchain/index.js
+++ functions/api/methods/interchain/index.js
@@ -1,9 +1,11 @@
 const interchainChart = require('./interchainChart');
+const interchainTotalVolume = require('./interchainTotalVolume');
 const interchainTotalFee = require('./interchainTotalFee');
 const interchainTotalActiveUsers = require('./interchainTotalActiveUsers');
 
 module.exports = {
   interchainChart,
+  interchainTotalVolume,
   interchainTotalFee,
   interchainTotalActiveUsers,
 };
