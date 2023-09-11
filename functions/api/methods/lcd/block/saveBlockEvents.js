d75e4cd484316935afa81fd4d7afbd1948bd002b
Sirapop Na Ranong <n.r.sirapop@gmail.com>
2023-09-11 11:19:56 +0700

    chore: normalize confirm_failed event

--- functions/api/methods/lcd/block/saveBlockEvents.js
+++ functions/api/methods/lcd/block/saveBlockEvents.js
@@ -1,24 +1,34 @@
+const moment = require('moment');
+
 const rpc = require('../../rpc');
 const { saveGMP } = require('../../gmp');
-const { toArray, normalizeQuote } = require('../../../utils');
+const { getChainKey } = require('../../../utils/config');
+const { toArray, toHex, normalizeQuote } = require('../../../utils');
 
 module.exports = async (lcd_response = {}) => {
   const { block } = { ...lcd_response };
   const { header } = { ...block };
-  const { height } = { ...header };
+  const { time } = { ...header };
+  let { height } = { ...header };
 
   if (height) {
+    height = Number(height);
     const { end_block_events } = { ...await rpc('/block_results', { height }) };
     const no_confirmed_events = toArray(end_block_events).filter(e => e.type?.includes('NoEventsConfirmed') && toArray(e.attributes).findIndex(a => ['tx_id'].includes(a.key)) > -1);
     for (const event of no_confirmed_events) {
       const { attributes } = { ...event };
-      const { chain, poll_id, tx_id } = { ...Object.fromEntries(toArray(attributes).map(a => [a.key, typeof a.value === 'string' ? normalizeQuote(a.value) : a.value])) };
+      const _attributes = Object.fromEntries(toArray(attributes).map(a => [a.key, typeof a.value === 'string' ? normalizeQuote(a.value) : a.value]));
+      const { poll_id } = { ..._attributes };
+      let { chain, tx_id } = { ..._attributes };
+      chain = getChainKey(chain) || chain;
+      tx_id = toHex(tx_id);
       await saveGMP(
         {
           event: 'confirm_failed',
           sourceTransactionHash: tx_id,
           poll_id,
           blockNumber: height,
+          block_timestamp: time ? moment(time).unix() : undefined,
           source_chain: chain,
         },
         chain,
