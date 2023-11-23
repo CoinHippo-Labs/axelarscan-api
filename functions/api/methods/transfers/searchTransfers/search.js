751d0ee9deeb55b4a06871a7fe2a46194557b260
Sirapop Na Ranong <n.r.sirapop@gmail.com>
2023-11-23 16:44:36 +0700

    chore: not write when cache hit

--- functions/api/methods/transfers/searchTransfers/search.js
+++ functions/api/methods/transfers/searchTransfers/search.js
@@ -12,16 +12,18 @@ module.exports = async (collection, query, params, delay_ms = 0, cache_id) => {
   const try_fetch_cache = !!(cache_id && aggs && toArray(must).length === (cache_id?.includes('status_') ? 1 : 0) && Object.keys({ ...bool }).filter(k => k !== 'must').length === 0);
 
   let output;
+  let cache_hit;
   if (try_fetch_cache) {
     const { data, updated_at } = { ...await get(CACHE_COLLECTION, cache_id) };
     if (data && updated_at && moment().diff(moment(updated_at), 'seconds') <= 300) {
       output = JSON.parse(data);
+      cache_hit = true;
     }
   }
   if (!output) {
     output = await read(collection, query, params);
   }
-  if (output && try_fetch_cache) {
+  if (output && try_fetch_cache && !cache_hit) {
     await write(CACHE_COLLECTION, cache_id, { data: JSON.stringify(output), updated_at: moment().valueOf() });
   }
   return output;
