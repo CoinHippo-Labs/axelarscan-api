2534f579a103c8f6dfbbbcc5db88edf875ca1662
Sirapop Na Ranong <n.r.sirapop@gmail.com>
2023-12-24 13:53:57 +0700

    chore: update set time_spent

--- utils/io.js
+++ utils/io.js
@@ -16,7 +16,7 @@ const finalizeResponse = (response, params, startTime = moment()) => {
   // on error, add parameters to response
   if (response?.error) response = { ...response, method: response.method || method, params: response.params || params };
   // add time spent to response
-  if (response && typeof response === 'object' && !Array.isArray(response) && !['getDataMapping'].includes(method)) {
+  if (response && typeof response === 'object' && !Array.isArray(response) && !['getGMPDataMapping', 'getTransfersDataMapping'].includes(method)) {
     response = { ...response, time_spent: moment().diff(startTime) };
   }
   return response;
