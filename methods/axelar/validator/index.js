eabf30212683ab9909a127feb9a75e4dd958fb55
Sirapop Na Ranong <n.r.sirapop@gmail.com>
2023-12-30 05:40:02 +0700

    chore: fwd validator methods

--- methods/axelar/validator/index.js
+++ methods/axelar/validator/index.js
@@ -6,4 +6,13 @@ const requestAPI = async params => await request(createInstance(getValidatorAPI(
 module.exports = {
   rpc: async params => await requestAPI({ ...params, method: 'rpc' }),
   lcd: async params => await requestAPI({ ...params, method: 'lcd' }),
+  searchBlocks: async params => await requestAPI({ ...params, method: 'searchBlocks' }),
+  searchTransactions: async params => await requestAPI({ ...params, method: 'searchTransactions' }),
+  searchUptimes: async params => await requestAPI({ ...params, method: 'searchUptimes' }),
+  searchProposedBlocks: async params => await requestAPI({ ...params, method: 'searchProposedBlocks' }),
+  searchHeartbeats: async params => await requestAPI({ ...params, method: 'searchHeartbeats' }),
+  searchPolls: async params => await requestAPI({ ...params, method: 'searchPolls' }),
+  getValidators: async params => await requestAPI({ ...params, method: 'getValidators' }),
+  getValidatorsVotes: async params => await requestAPI({ ...params, method: 'getValidatorsVotes' }),
+  getChainMaintainers: async params => await requestAPI({ ...params, method: 'getChainMaintainers' }),
 };
