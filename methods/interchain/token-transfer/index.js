1c2c65aea268a042f0ef7457b7ec80042623cf4c
Sirapop Na Ranong <n.r.sirapop@gmail.com>
2024-01-10 12:32:51 +0700

    chore: use lcd/rpc from validator api

--- methods/interchain/token-transfer/index.js
+++ methods/interchain/token-transfer/index.js
@@ -4,8 +4,6 @@ const { createInstance, request } = require('../../../utils/http');
 const requestAPI = async params => await request(createInstance(getTokenTransferAPI(), { timeout: 30000 }), { params });
 
 module.exports = {
-  rpc: async params => await requestAPI({ ...params, method: 'rpc' }),
-  lcd: async params => await requestAPI({ ...params, method: 'lcd' }),
   transfersChart: async params => await requestAPI({ ...params, method: 'transfersChart' }),
   transfersTotalVolume: async params => await requestAPI({ ...params, method: 'transfersTotalVolume' }),
   transfersTotalFee: async params => await requestAPI({ ...params, method: 'transfersTotalFee' }),
