3045e2063b597445336131027d1f4db6d232e981
Sirapop Na Ranong <n.r.sirapop@gmail.com>
2024-01-28 10:25:16 +0700

    chore: update using httpAgent

--- utils/http.js
+++ utils/http.js
@@ -9,7 +9,7 @@ const createInstance = (url, options) => {
   let { timeout, headers } = { ...options };
   timeout = timeout || 5000;
   if (gzip) headers = { ...headers, 'Accept-Encoding': 'gzip' };
-  const httpsAgent = url.includes('amazonaws.com') ? new https.Agent({ keepAlive: true }) : undefined;
+  const httpsAgent = url.includes('execute-api.us-east-2.amazonaws.com') ? new https.Agent({ keepAlive: true }) : undefined;
   return axios.create({ ...options, baseURL: url, timeout, headers, httpsAgent });
 };
 
